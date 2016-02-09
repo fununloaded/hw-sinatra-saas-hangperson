53100350b5d7356a343ec4dfa61618484e42da55
Rowan Sandhu <rowan.sandhu@berkeley.edu>
2016-02-09 07:56:21 +0000

    wow so much more

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,8 +10,57 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
+  attr_accessor :word, :wrong_guesses, :guesses
+  
+  def guess(letter)
+    if !(letter =~ /[a-zA-Z]/)
+      raise ArgumentError
+    end
+    letter.each_char do |i|
+      if (@guesses.include? i) || (@wrong_guesses.include? i) || (/[[:upper:]]/.match(i))
+        return false
+      else
+        if @word.include? i
+          @guesses << i
+        else
+          @wrong_guesses << i
+        end
+      end
+    end
+  end
+  
+  def self.guesses
+    puts @guesses
+  end
+  
+  def self.wrong_guesses
+    puts @wrong_guesses
+  end
+  
+  def word_with_guesses
+    format_word = @word
+    format_word.each_char do |i|
+      if !(@guesses.include? i)
+        format_word.gsub!(i, "-")
+      end
+    end
+    return format_word
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.length == 7
+      return :lose
+    end
+    if (guesses.size == @word.size) && (guesses.each_char.sort == @word.each_char.sort)
+      return :win
+    end
+    return :play
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
